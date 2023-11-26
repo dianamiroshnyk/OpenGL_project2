70bedab9f9eee2e88fa74a8a1e5d9bd6cda313ac
dianamiroshnyk <dianamirosnik2@gmail.com>
2023-11-26 22:35:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opengl_project2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.opengl_project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView myGLView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myGLView = new GLSurfaceView(this);
+        myGLView.setEGLContextClientVersion(3);
+        myGLView.setRenderer((new MyGLRenderer()));
+        setContentView(myGLView);
+
+    }
+}
---
+++ app/src/main/java/com/example/opengl_project2/MyGLRenderer.java
@@ -0,0 +1,62 @@
+package com.example.opengl_project2;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+import android.opengl.GLES31;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private final float[] vPMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
+
+    private Triangle mTriangle;
+    private Square mSquare;
+
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES31.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        mTriangle = new Triangle();
+        mSquare = new Square();
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        // Redraw background color
+        GLES31.glClear(GLES31.GL_COLOR_BUFFER_BIT);
+
+        //set camera position
+        Matrix.translateM(viewMatrix, 0, -1.5f, 1.5f, -5.0f);
+        float[] mvpMatrix = new float[16];
+        //multiply projection and view matrix
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        mSquare.draw(vPMatrix);
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, 3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        // Draw shape
+        mTriangle.draw(vPMatrix);
+    }
+
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES31.glViewport(0, 0, width, height);
+        float ratio = (float) width / height;
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+
+    }
+
+    public static int loadShader(int type, String shaderCode){
+        // create a vertex shader type (GLES31.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES31.GL_FRAGMENT_SHADER)
+        int shader = GLES31.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES31.glShaderSource(shader, shaderCode);
+        GLES31.glCompileShader(shader);
+
+        return shader;
+    }
+}
